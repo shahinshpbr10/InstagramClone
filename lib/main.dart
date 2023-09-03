4a9eb11e62a917b76cfb9fd21c29c0ae8f753b63
SHAHINSH <123875287+shahinshpbr10@users.noreply.github.com>
2023-09-03 14:45:55 +0530

    Firebase Setup Done for IOS ,android and Web

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,26 @@
+import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/Responsive/mobile_layout.dart';
 import 'package:instagram_clone/Responsive/responsive_layout.dart';
 import 'package:instagram_clone/Responsive/web_layout.dart';
 import 'package:instagram_clone/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyAAkqwOWRAqkREQzYnTJrLzcpI-D06iUC8",
+          appId: "instagramclone-66d7a.firebaseapp.com",
+          messagingSenderId: "527043410946",
+          projectId: "instagramclone-66d7a",
+          storageBucket: "instagramclone-66d7a.appspot.com"),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
